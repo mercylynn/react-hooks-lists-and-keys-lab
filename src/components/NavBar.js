86cc6354094e4ab699d4cd249a1504666027c76d
mercylynn <mercy.kamene@student.moringaschool.com>
2022-09-23 11:07:25 +0300

    Done

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,15 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
+  const linkList = links.map((link) => {
+    return <a key={link} href={`#${link}`}>{link}</a>
+  })
+  return <nav>
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+
+    {linkList}
+  </nav >;
 }
+
 
 export default NavBar;
